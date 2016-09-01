f7818e35368ed4ac165bb93fe661e3455c2ac25a
bitLiuYang <674837074@qq.com>
2016-09-01 23:02:32 +0800

    create dataset, create num vector

--- 03_bayes/bayes.py
+++ 03_bayes/bayes.py
@@ -4,3 +4,34 @@ Created on Mon Aug 22 14:25:43 2016
 
 @author: Jack
 """
+
+def loadDataSet():
+    text_set = [
+        ['Dog', 'is', 'spam', 'OK'],
+        ['Cat', 'is', 'very', 'good'],
+        ['Human', 'are', 'very', 'smart'],
+        ['Money', 'is', 'very', 'cheap']    
+    ] 
+    label = [0,0,0,1]
+    return text_set,label
+    
+def createVocabList(dataSet):
+    vocab = set([]) #使用set类型，保证没个单词只出现一次
+    for document in dataSet:
+        vocab = vocab | set(document)
+    return list(vocab)
+    
+def setOfWords2Vec(vocabList, inputSet):
+    ret_vec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            ret_vec[vocabList.index(word)] = 1
+        else:
+            print('the word : %s is not in vocabList' % word)
+    return ret_vec
+    
+listText, Label = loadDataSet()
+setText = createVocabList(listText)
+print(setText)
+list_in = setOfWords2Vec(setText, listText[3])
+print(list_in)
